dce86f57e584de0e2c0151a14c01cb8e6a38f694
Hummart <oscar.norden@gmail.com>
2017-12-07 01:12:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var pointo = 0;
+
+function addKuriku() {
+
+    pointo++;
+    
+    document.getElementById("pointosu").innerHTML = "Du har " + pointo + " harempoäng";
+    
+}
+
